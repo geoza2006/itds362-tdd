223758418a4b03e211986286be4243bbd824129e
Gao <gaoxme2006@gmail.com>
2026-09-03 21:33:18 +0700

    A5: refactor quantity tests for units

--- test_kitchen.py
+++ test_kitchen.py
@@ -10,23 +10,37 @@
 from kitchen import Quantity
 
 
-def test_multiplication():
-    flour = Quantity(200)
+def grams(amount):
+    return Quantity(amount, "g")
 
-    assert flour.times(3) == Quantity(600)
+
+def ounces(amount):
+    return Quantity(amount, "oz")
+
+
+def test_multiplication():
+    flour = grams(200)
+
+    assert flour.times(3) == grams(600)
 
 
 def test_multiplication_by_two():
-    flour = Quantity(200)
+    flour = grams(200)
 
-    assert flour.times(2) == Quantity(400)
+    assert flour.times(2) == grams(400)
 
 
 def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
+    flour = grams(200)
 
-    assert flour.times(3) == Quantity(600)
-    assert flour.times(2) == Quantity(400)
+    assert flour.times(3) == grams(600)
+    assert flour.times(2) == grams(400)
+
+
+def test_equality():
+    assert grams(200) == grams(200)
+    assert grams(200) != grams(300)
+
 
 def test_grams_are_not_ounces():
-    assert Quantity(1, "g") != Quantity(1, "oz")
+    assert grams(1) != ounces(1)
