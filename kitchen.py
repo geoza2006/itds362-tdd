8cda9cc005a2d2722f4ba6fddcd60e6c4f7bc1b2
Gao <gaoxme2006@gmail.com>
2026-09-03 21:38:45 +0700

    A6: converter reduce unit

--- kitchen.py
+++ kitchen.py
@@ -6,14 +6,14 @@ class Quantity:
     def times(self, multiplier):
         return Quantity(self.amount * multiplier, self.unit)
 
+    def plus(self, other):
+        return Sum(self, other)
+
     def __eq__(self, other):
         return self.amount == other.amount and self.unit == other.unit
 
     def __repr__(self):
         return f"Quantity({self.amount}, {self.unit!r})"
-
-    def plus(self, other):
-        return Sum(self, other)
 
 
 class Sum:
@@ -21,11 +21,23 @@ class Sum:
         self.left = left
         self.right = right
 
-    def reduce(self, unit):
-        return Quantity(
-            self.left.amount + self.right.amount,
-            unit
+    def times(self, multiplier):
+        return Sum(
+            self.left.times(multiplier),
+            self.right.times(multiplier)
         )
+
+    def reduce(self, unit):
+        left_amount = self.left.amount
+        right_amount = self.right.amount
+
+        if self.left.unit == "g" and self.right.unit == "oz":
+            right_amount = self.right.amount * 28.3495
+
+        if self.left.unit == "oz" and self.right.unit == "g":
+            left_amount = self.left.amount * 28.3495
+
+        return Quantity(left_amount + right_amount, unit)
 
 
 class Converter:
